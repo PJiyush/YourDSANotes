8bad51422361e423c453844f0ee289a18f28bb72
PJiyush <jhamnani.1@iitj.ac.in>
2023-12-27 22:20:07 +0530

    setup the project

---
+++ YourDSANotes/src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { Button } from './components/Buttons'
+
+function App() {
+  return (
+    <div>
+      hello
+    </div>
+  )
+}
+
+export default App
